1bd2fe02dfe4e33aa40475cb76e911ebbd68699a
mc4tech <chavezmeguel@yahoo.com>
2017-07-31 21:30:36 -0400

    create prod table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
+var Table = require('cli-table');
 var connection = mysql.createConnection({
 	host: "localhost",
 	port: 3306,
@@ -16,6 +17,22 @@ connection.connect(function(err) {
 function displayProducts() {
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
-    console.log(res);
+    // console.log(res);
+    
+    // instantiate 
+	var table = new Table({
+	    head: ['Item Id', 'Product', 'Price']
+	  , colWidths: [20, 20, 10]
+	});
+
+	//loops through the response and pushes product info to the table 
+    for (var i = 0; i < res.length; i++) {
+    	// table is an Array, so you can `push`, `unshift`, `splice` and friends 
+		table.push(
+		    [res[i].item_id, res[i].product_name, "$" + res[i].cust_price]
+		);
+    }
+    console.log("\n" + table.toString() + "\n");
+    console.log("-----------------------------------");
   });
 }
